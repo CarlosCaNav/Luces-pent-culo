ee98492d3eb319054740e0d7f0b23b54aabf5dd9
Carlos <Nitro@localhost.localdomain>
2024-02-19 13:56:20 +0100

    Código más limpio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,88 +24,29 @@ export class AppComponent {
 
     const botones_a_cambiar = this.interruptores_cambiar[interruptor];
 
-    // Version 1
-    // for (var i = 0; i < contrarios.length; i++) {
-    //   const boton_contrario = contrarios[i];
-
-    //   this.interruptores[boton_contrario] = !this.interruptores[boton_contrario];
-    // }
-
-    // Version 2
     botones_a_cambiar.forEach((interruptor) => {
       this.interruptores[interruptor] = !this.interruptores[interruptor];
     });
-
-
-    // Version antigua
-    // if (interruptor == 1) {
-    //   this.interruptores[0] = !this.interruptores[0]
-    //   this.interruptores[4] = !this.interruptores[4]
-    //   this.interruptores[3] = !this.interruptores[3]
-    // }
-    // else if (interruptor == 2) {
-    //   this.interruptores[1] = !this.interruptores[1]
-    //   this.interruptores[2] = !this.interruptores[2]
-    //   this.interruptores[4] = !this.interruptores[4]
-    // }
-    // else if (interruptor == 3) {
-    //   this.interruptores[2] = !this.interruptores[2]
-    //   this.interruptores[1] = !this.interruptores[1]
-    //   this.interruptores[3] = !this.interruptores[3]
-    // }
-    // else if (interruptor == 4) {
-    //   this.interruptores[3] = !this.interruptores[3]
-    //   this.interruptores[0] = !this.interruptores[0]
-    //   this.interruptores[2] = !this.interruptores[2]
-    // }
-    // else if (interruptor == 5) {
-    //   this.interruptores[4] = !this.interruptores[4]
-    //   this.interruptores[1] = !this.interruptores[1]
-    //   this.interruptores[0] = !this.interruptores[0]
-    // };
-
     this.intentos++;
 
   }
 
-  reiniciar(modo: string) {
+  reiniciaraleatorio(modo: boolean) {
 
-    if (modo == "normal") {
-      for (var i = 0; i < this.interruptores.length; i++) {
-        this.interruptores[i] = false;
-        console.log("modo normal");
-      }
-    }
-    else if (modo == "aleatorio") {
+
+    if (modo) {
       for (var i = 0; i < this.interruptores.length; i++) {
 
         var numeroAleatorio = Math.floor(Math.random() * (2));
 
         this.interruptores[i] = numeroAleatorio == 1;
       }
-
-
-
-
-      /* 
-          else if (modo == "aleatorio") {
-            var NumeroAleatorio = 0;
-            var booleanAleatorio = true;
-      
-      
-          for (var i = 0; i < this.interruptores.length; i++ ,
-            NumeroAleatorio = Math.floor(Math.random() * (2) + 1)) {
-                  this.interruptores[i] = booleanAleatorio;
-                  if (NumeroAleatorio == 1) {
-                    booleanAleatorio = true;
-                  }
-                  else if (NumeroAleatorio == 2) {
-                    booleanAleatorio = false
-                  } */
-
-
-
-      console.log("modo aleatorio");
+    }
+      else {
+        for (var i = 0; i < this.interruptores.length; i++) {
+          this.interruptores[i] = false;
+          console.log("modo normal");
+        }
 
     };
 
