9de549ac768f4d4e784187288721917ea636eb73
Carlos Carretero <carloscn90@gmail.com>
2024-10-25 15:44:21 +0200

    primera versión completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,32 +10,44 @@ export class AppComponent {
 
   interruptores = [false, false, false, false, false];
 
+fin = false;
+
   interruptores_cambiar = [
-    [0, 3, 4], // interruptor 0
-    [1, 2, 4], // interruptor 1 
-    [2, 1, 3], // interruptor 2
-    [3, 0, 2], // interruptor 3
-    [4, 0, 1], // interruptor 4
+    [3, 4], // interruptor 0
+    [2, 4], // interruptor 1 
+    [1, 3], // interruptor 2
+    [0, 2], // interruptor 3
+    [0, 1], // interruptor 4
   ];
 
   intentos: number = 0;
 
   boton(interruptor: number) {
-
-    console.log(interruptor);
-    
-
     const botones_a_cambiar = this.interruptores_cambiar[interruptor];
 
-    botones_a_cambiar.forEach((interruptor) => {
-      this.interruptores[interruptor] = !this.interruptores[interruptor];
-    });
+
+    this.interruptores[interruptor] = !this.interruptores[interruptor];
+    
+
+    setTimeout(() => {
+    botones_a_cambiar.forEach((cambio) => {
+      this.interruptores[cambio] = !this.interruptores[cambio];})
+      
+      if(this.interruptores[0] && this.interruptores[1] && this.interruptores[2] && 
+        this.interruptores[3] && this.interruptores[4]){
+          this.fin = true
+        }
+    ;
+}, 100)
+
+
     this.intentos++;
 
   }
 
   reiniciaraleatorio(modo: boolean) {
 
+    this.fin = false
 
     if (modo) {
       for (var i = 0; i < this.interruptores.length; i++) {
